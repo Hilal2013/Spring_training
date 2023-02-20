0bffa42741f2da5d7054338b61f3df05008e9a1b
Hilal2013 <hilalozkul@gmail.com>
2023-02-20 17:09:16 -0500

    added ComputerTest class

--- spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/ComputerTest.java
+++ spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/ComputerTest.java
@@ -1,7 +1,13 @@
 package com.cydeo.bean_annotation;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class ComputerTest {
     public static void main(String[] args) {
+        //Creating container by using Application Context
+        ApplicationContext container = new AnnotationConfigApplicationContext();
+
 
     }
 }
