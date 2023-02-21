c7b2d13d996668e66f76846894e3c6851e4bca48
Hilal2013 <hilalozkul@gmail.com>
2023-02-21 18:03:14 -0500

    updated

--- spring-04-Autowiring/src/main/java/com/cydeo/streotype_annotation/casefactory/Dimensions.java
+++ spring-04-Autowiring/src/main/java/com/cydeo/streotype_annotation/casefactory/Dimensions.java
@@ -3,10 +3,12 @@ package com.cydeo.streotype_annotation.casefactory;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.stereotype.Component;
 
 @AllArgsConstructor
 @Getter
 @Setter
+@Component
 public class Dimensions {
     private int width;
     private int height;
