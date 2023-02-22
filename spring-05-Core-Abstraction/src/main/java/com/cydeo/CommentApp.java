f127a7fcc3e9aad612dd2aae5a0d540481e41e9c
Hilal2013 <hilalozkul@gmail.com>
2023-02-22 12:08:09 -0500

    added CommentApp Runner file

---
+++ spring-05-Core-Abstraction/src/main/java/com/cydeo/CommentApp.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CommentApp {
+    public static void main(String[] args) {
+        ApplicationContext container=new AnnotationConfigApplicationContext();
+        //All of them my classes so I  wanna go with streotype annotation
+       //which one I need to annotate with @ component
+
+    }
+}
