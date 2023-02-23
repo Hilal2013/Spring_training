7ab134e378d8a7cbbeddeab1f207091a49917d67
Hilal2013 <hilalozkul@gmail.com>
2023-02-23 15:19:23 -0500

    added files

--- spring-07-SpringBoot-Demo/src/main/java/com/cydeo/CydeoApplication.java
+++ spring-07-SpringBoot-Demo/src/main/java/com/cydeo/CydeoApplication.java
@@ -2,13 +2,18 @@ package com.cydeo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class CydeoApplication {
 
 	public static void main(String[] args) {
+//	ApplicationContext container = SpringApplication.run(CydeoApplication.class, args);
+		SpringApplication.run(CydeoApplication.class, args);//static method so in man method we can use
 
-		SpringApplication.run(CydeoApplication.class, args);
+
+
+
 	}
 
 }
---
+++ spring-07-SpringBoot-Demo/src/main/java/com/cydeo/service/CommentService.java
@@ -0,0 +1,33 @@
+package com.cydeo.service;
+
+import com.cydeo.model.Comment;
+import com.cydeo.proxy.CommentNotificationProxy;
+import com.cydeo.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+public class CommentService {
+private final CommentRepository commentRepository;//putting final is good practice->reason is that
+    //you might forget creating constructor if we put final->compile error->these arent initiliazed yet
+private final CommentNotificationProxy commentNotificationProxy;
+
+    public CommentService(CommentRepository commentRepository,@Qualifier("PUSH") CommentNotificationProxy commentNotificationProxy) {
+     //I put the default Bean Name
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
+
+    public void publishComment(Comment comment){
+        //save in the database
+        //send email
+//to be able to call method this method belongs to class//tightly coupled call interface
+        commentRepository.storeComment(comment);
+        commentNotificationProxy.sendComment(comment);
+        //why interface//maybe tomorrow not send email  I will push notification
+    }
+
+}
