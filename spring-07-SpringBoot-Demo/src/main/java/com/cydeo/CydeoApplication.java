9fdfd1b608918c779fd4eb7e0f67be5ab8da2255
Hilal2013 <hilalozkul@gmail.com>
2023-02-26 12:25:20 -0500

    updated

--- spring-07-SpringBoot-Demo/src/main/java/com/cydeo/CydeoApplication.java
+++ spring-07-SpringBoot-Demo/src/main/java/com/cydeo/CydeoApplication.java
@@ -5,30 +5,34 @@ import com.cydeo.service.CommentService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CydeoApplication {
 
-	public static void main(String[] args) {
-	ApplicationContext container = SpringApplication.run(CydeoApplication.class, args);
-		//SpringApplication.run(CydeoApplication.class, args);//static method so in man method we can use
+    public static void main(String[] args) {
+        ApplicationContext container = SpringApplication.run(CydeoApplication.class, args);
+        //SpringApplication.run(CydeoApplication.class, args);//static method so in man method we can use
 
-		Comment comment  = new Comment();
-		comment.setAuthor("Johnson");
-		comment.setText("Spring Framework");
-		CommentService commentService = container.getBean(CommentService.class);
-		commentService.publishComment(comment);
-	//	Storing comment: Spring Framework
-	//	Sending notification for comment: Spring Framework
-		commentService.printConfigData();
-	    //hilal
-		//abc123
-		//www.cydeo.com
-		commentService.printDbConfigData();
-		//Ozzy
-		//abc123
-		//[oracle, postgres, amazon]
-	}
-
+        Comment comment = new Comment();
+        comment.setAuthor("Johnson");
+        comment.setText("Spring Framework");
+        CommentService commentService = container.getBean(CommentService.class);
+        commentService.publishComment(comment);
+        //	Storing comment: Spring Framework
+        //	Sending notification for comment: Spring Framework
+        commentService.printConfigData();
+        //hilal
+        //abc123
+        //www.cydeo.com
+        commentService.printDbConfigData();
+        //Ozzy
+        //abc123
+        //[oracle, postgres, amazon]
+    }
+@Bean
+    public String stringBean() {
+        return "Cydeo";
+    }
 
 }
