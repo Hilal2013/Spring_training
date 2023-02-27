0f12ef1a72207879f872faa2e2859a40ab270154
Hilal2013 <hilalozkul@gmail.com>
2023-02-27 16:25:42 -0500

    updated

--- spring-08-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
+++ spring-08-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
@@ -5,9 +5,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    @RequestMapping("/home")
+    @RequestMapping("/home")//localhost:8080/home  write your chrome
     public String home(){//not important mehodName//this method will work which endpoint?
-        //www.amazon.com/register
+        //www.amazon.com/register//
         return"home.html";
     }
+
+    @RequestMapping("/ozzy") //localhost:8080/ozzy  write your chrome
+    public String getHomePage(){
+
+        return"home.html";
+    }
+    @RequestMapping //if you dont pu anything or just / still works as default
+    public String getHome(){
+
+        return"home.html";
+    }
+
+    @RequestMapping ({"/apple","/orange"})//same vieww can come with this couple of endpoints
+    public String get(){
+
+        return"home.html";
+    }
+
 }
