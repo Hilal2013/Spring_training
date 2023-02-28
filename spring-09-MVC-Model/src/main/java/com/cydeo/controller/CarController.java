6bb8760c368f20abadc4660cd80e5595d37e1066
Hilal2013 <hilalozkul@gmail.com>
2023-02-28 17:18:04 -0500

    updated

--- spring-09-MVC-Model/src/main/java/com/cydeo/controller/CarController.java
+++ spring-09-MVC-Model/src/main/java/com/cydeo/controller/CarController.java
@@ -2,6 +2,7 @@ package com.cydeo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 @Controller
@@ -20,10 +21,19 @@ return "car/car-info";
     }
     //localhost:8080/info3?make=Honda&year=2015
     @RequestMapping("/info3")
-    public String carInfo3(@RequestParam String make,int year, Model model){
+    public String carInfo3(@RequestParam String make,@RequestParam int year, Model model){
 
         model.addAttribute("make",make);
         model.addAttribute("year",year);
         return "car/car-info";
     }
+    //through path variable {} for dynamic
+  //  localhost:8080/info/honda/2015
+    @RequestMapping("/info/{make}/{year}")
+    public String getCarInfo(@PathVariable String make,@PathVariable int year){
+        System.out.println(make);
+        System.out.println(year);
+        return "car/car-info";
+    }
+
 }
