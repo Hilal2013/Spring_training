bd322f6e6c5903a1b80ad1ccb4f1c26d5a4a2d55
Hilal2013 <hilalozkul@gmail.com>
2023-02-28 15:23:19 -0500

    updated

--- spring-09-MVC-Model/src/main/java/com/cydeo/controller/CarController.java
+++ spring-09-MVC-Model/src/main/java/com/cydeo/controller/CarController.java
@@ -1,12 +1,29 @@
 package com.cydeo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
+@Controller
 public class CarController {
 @RequestMapping("/info")
-    public String carInfo(){
-
+    public String carInfo(@RequestParam String make, Model model){
+//Iwanna serch something//How we will capture it from user interface
+model.addAttribute("make",make);
+return "car/car-info";
+    }
+    @RequestMapping("/info2")
+    public String carInfo2(@RequestParam(value = "make",required = false,defaultValue ="KIA") String make, Model model){
+//Iwanna serch something//How we will capture it from user interface
+        model.addAttribute("make",make);
         return "car/car-info";
     }
+    //localhost:8080/info3?make=Honda&year=2015
+    @RequestMapping("/info3")
+    public String carInfo3(@RequestParam String make,int year, Model model){
 
+        model.addAttribute("make",make);
+        model.addAttribute("year",year);
+        return "car/car-info";
+    }
 }
