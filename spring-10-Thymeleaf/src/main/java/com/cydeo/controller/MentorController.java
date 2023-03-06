64216a20e058e7f6624fe2cee36954ab345e6b85
Hilal2013 <hilalozkul@gmail.com>
2023-03-06 13:05:25 -0500

    updated

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -1,11 +1,14 @@
 package com.cydeo.controller;
-
-import com.cydeo.bootstrap.DataGenerator;
 import com.cydeo.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/mentor")
@@ -14,14 +17,21 @@ public class MentorController {
     @GetMapping("/register") //localhost:8080/mentor/register
     public String register(Model model){
 
+        List<String> batchList = Arrays.asList("JD1","JD2","JD3");
+        model.addAttribute("batchList",batchList);
+
         model.addAttribute("mentor",new Mentor());
-        //lets give this object to view through attribute
-        //this mentor is holding new empty object//i need to pass this objet to view
-        // object needs to be available in the form
 
-        //    List<String> batchList = Arrays.asList("JD1","JD2","JD3");
         return "mentor/mentor-register";
-
     }
+
+    @PostMapping("/confirm")
+    public String submitForm(@ModelAttribute("mentor") Mentor mentor){
+
+
+    //   return "mentor/mentor-confirmation";
+        return "redirect:/mentor/register";
+    }
+
 
 }
--- spring-10-Thymeleaf/src/main/java/com/cydeo/model/Mentor.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/model/Mentor.java
@@ -1,11 +1,10 @@
 package com.cydeo.model;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class Mentor {
     private String firstName;
