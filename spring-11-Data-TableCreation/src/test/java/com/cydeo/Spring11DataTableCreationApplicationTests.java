444ab0f3a00a281bdc6a6492c3960ae1039733dd
Hilal2013 <hilalozkul@gmail.com>
2023-04-03 17:42:24 -0400

    created spring-11-data-tableCreation module

--- spring-11-Data-TableCreation/src/test/java/com/cydeo/Spring11DataTableCreationApplicationTests.java
+++ spring-11-Data-TableCreation/src/test/java/com/cydeo/Spring11DataTableCreationApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Spring11DataTableCreationApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
