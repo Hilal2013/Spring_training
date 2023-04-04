ac669492cabe02038389d955871e13ce70464d27
Hilal2013 <hilalozkul@gmail.com>
2023-04-04 11:16:27 -0400

    updated

--- spring-11-Data-TableCreation/src/main/resources/data.sql
+++ spring-11-Data-TableCreation/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO EMPLOYEES(id,name)
 VALUES(1,'Mike');
 INSERT INTO students(first_name,last_name,email)
-VALUES(1,'Mike','Smith','mike@cydeo.com');
+VALUES('Mike','Smith','mike@cydeo.com');
 
 INSERT INTO students(first_name,last_name,email)
-VALUES(2,'Tom','Hanks','tom@cydeo.com');
+VALUES('Tom','Hanks','tom@cydeo.com');
