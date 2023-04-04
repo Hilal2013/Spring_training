594b0a5022e7d4dcb4f754f90310a74241f3b5ee
Hilal2013 <hilalozkul@gmail.com>
2023-04-04 14:20:43 -0400

    updated

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/repository/CarRepository.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/repository/CarRepository.java
@@ -2,7 +2,9 @@ package com.cydeo.repository;
 
 import com.cydeo.Entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CarRepository extends JpaRepository<Car,Long> {
     //my car repository has now 25 methods
     //why we used Car, Long=>why interface is looking those?
