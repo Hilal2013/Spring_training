b3cde751797ec50dfc0856597897e9c7bfac4d79
Hilal2013 <hilalozkul@gmail.com>
2023-04-04 14:20:40 -0400

    updated

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/Entity/Employee.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/Entity/Employee.java
@@ -1,11 +1,23 @@
 package com.cydeo.Entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import com.cydeo.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
-public class Employee {
-    @Id
-    private int id;
-    private String name;
+@NoArgsConstructor// you should use
+@AllArgsConstructor //lombok doesnt get parent field =>id
+@Table(name = "employees")
+public class Employee extends BaseEntity{
+    private String firstName;//column name will befirst_name
+    private String lastName;
+    private String email;
+    @Column(columnDefinition = "DATE")
+    private LocalDate hireDate;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+    private int salary;
 }
