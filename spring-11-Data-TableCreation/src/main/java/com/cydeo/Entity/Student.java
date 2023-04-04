3b1fa43aac30a2e5cb8ff731bf264dec9d5cbf07
Hilal2013 <hilalozkul@gmail.com>
2023-04-04 09:09:07 -0400

    Student entity

---
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/Entity/Student.java
@@ -0,0 +1,13 @@
+package com.cydeo.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+    @Id
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
