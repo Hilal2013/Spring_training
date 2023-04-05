0ab89eeaeca02e38484e67b51a7557c264d1fa70
Hilal2013 <hilalozkul@gmail.com>
2023-04-05 14:48:56 -0400

    update

--- spring-12-Data-Relationship/src/main/java/com/cydeo/entity/Cart.java
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/entity/Cart.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
@@ -12,5 +13,9 @@ public class Cart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @ManyToMany
+    @JoinTable(name = "cart_item_rel",
+            joinColumns = @JoinColumn(name = "c_id"),
+            inverseJoinColumns = @JoinColumn(name = "i_id"))
+    private List<Item> itemList;
 }
