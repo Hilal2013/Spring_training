08425e2eee6433cbb483156d3452fa1b2008dd37
Hilal2013 <hilalozkul@gmail.com>
2023-04-12 09:25:58 -0400

    updated

--- spring-13-Data-Queries/src/main/java/com/cydeo/repository/CourseRepository.java
+++ spring-13-Data-Queries/src/main/java/com/cydeo/repository/CourseRepository.java
@@ -3,6 +3,7 @@ package com.cydeo.repository;
 import com.cydeo.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.stream.Stream;
@@ -25,7 +26,7 @@ public interface CourseRepository extends JpaRepository<Course,Long> {
 
     //Find all courses by category and returns a stream
     Stream<Course> streamAllByCategory(String category);
-@Query("")
-    List<Course> retrieveAllByCategoryAndRatingGreaterThan();
+@Query("select c from Course c where c.category=:category and c.rating>:rating")
+    List<Course> retrieveAllByCategoryAndRatingGreaterThan(@Param("category")String category,@Param("rating") int rating);
 
 }
