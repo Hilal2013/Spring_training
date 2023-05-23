0b9b4c8aec0ced430c8e5c3906e27a543ca7b877
Hilal2013 <hilalozkul@gmail.com>
2023-05-23 13:29:07 -0400

    ResponseWrapper

---
+++ spring-17-REST-ConsumingAPIs/src/main/java/com/cydeo/dto/ResponseWrapper.java
@@ -0,0 +1,36 @@
+package com.cydeo.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ResponseWrapper {
+    private boolean success;
+    private String message;
+    private Integer code;
+
+    //dont forget data
+
+    private Object data;
+   public ResponseWrapper(String message, Object data){
+
+      this.message=message;
+      this.data=data;
+       this.code= HttpStatus.OK.value();
+       this.success=true;
+   }
+    public ResponseWrapper(String message){
+
+        this.message=message;
+        this.data=data;
+        this.code= HttpStatus.OK.value();
+        this.success=true;
+    }
+//in two different ways  with data and without data
+    //if I want to delete I wont pass data//im na gonna see data in json output //for delete im gonna use this constructor
+
+}
