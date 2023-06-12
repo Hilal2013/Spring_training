0ffc612bf004756b29d6e69243a746d28fe4d699
Hilal2013 <hilalozkul@gmail.com>
2023-06-12 12:16:27 -0400

    created new AOP module

--- spring-21-AOP/src/main/java/com/cydeo/Spring21AopApplication.java
+++ spring-21-AOP/src/main/java/com/cydeo/Spring21AopApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Spring17RestConsumeApplication {
+public class Spring21AopApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Spring17RestConsumeApplication.class, args);
+        SpringApplication.run(Spring21AopApplication.class, args);
     }
 
 }
--- spring-21-AOP/src/test/java/com/cydeo/Spring21AopApplicationTests.java
+++ spring-21-AOP/src/test/java/com/cydeo/Spring21AopApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Spring17RestConsumeApplicationTests {
+class Spring21AopApplicationTests {
 
     @Test
     void contextLoads() {
